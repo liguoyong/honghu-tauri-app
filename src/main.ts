87c28471ca93eaf8be2452ed45804378c7911780
ex_guoyong.li <ex_guoyong.li@tcl.com>
2023-08-20 16:26:48 +0800

    优化登录首页

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,29 @@
 import { createApp } from "vue";
-import "./styles.css";
 import App from "./App.vue";
+import router from './route'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import "./styles.css";
+import '@/assets/iconfont/iconfont.css';
+import '@/assets/iconfont/iconfont.js';
+import { createPinia } from 'pinia'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import 'element-plus/theme-chalk/dark/css-vars.css' //这句是暗黑模式切换
+import '@/style/element/dark/css-vars.css'
+import '@/style/theme.css'
+import '@/style/index.scss'
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+import i18n from './lang/index'
+import 'virtual:svg-icons-register'
+// import '@/icons' // icon
+import globalComponent from '@/components/index';
 
-createApp(App).mount("#app");
+const app = createApp(App)
+app.use(createPinia()).use(router).use(ElementPlus, {
+    locale: zhCn,
+}).use(i18n)
+app.use(globalComponent)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
+app.mount("#app");
--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,47 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-
+import { fileURLToPath, URL } from 'node:url';
+import viteSvgIcons, { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
+import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [vue()],
-
+  plugins: [vue(),
+  //引入SVG图标素材文件
+  createSvgIconsPlugin({
+    iconDirs: [path.resolve(process.cwd(), 'src/icons/svg')],
+    symbolId: '[name]',
+  })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        //自定义的主题文件
+        additionalData: `@use "@/style/element/index.scss" as *;`,
+      },
+    },
+  },
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
-  // 1. prevent vite from obscuring rust errors
+  // prevent vite from obscuring rust errors
   clearScreen: false,
-  // 2. tauri expects a fixed port, fail if that port is not available
+  // tauri expects a fixed port, fail if that port is not available
   server: {
     port: 1420,
     strictPort: true,
   },
-  // 3. to make use of `TAURI_DEBUG` and other env variables
+  // to make use of `TAURI_DEBUG` and other env variables
   // https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
   envPrefix: ["VITE_", "TAURI_"],
+  build: {
+    // Tauri supports es2021
+    target: "esnext",
+    // don't minify for debug builds
+    minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
+    // produce sourcemaps for debug builds
+    sourcemap: !!process.env.TAURI_DEBUG,
+  },
 }));
