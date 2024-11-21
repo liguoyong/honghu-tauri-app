59f343d9d2b830a0eedb9f08acb23abe765ce4f8
liguoyong <424465344@qq.com>
2024-11-22 01:23:03 +0800

    增加文件

---
+++ src/components/TipsTooltip/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <el-tooltip
+    popper-class="tips-tooltip-main"
+    :placement="placement"
+    :title="title"
+    :width="width"
+    :content="content"
+    v-bind="$attrs"
+  >
+    <template #content>
+      <slot />
+    </template>
+    <slot name="trigger">
+      <el-icon><QuestionFilled /></el-icon>
+    </slot>
+  </el-tooltip>
+</template>
+<script>
+export default {
+  name: 'TipsPopover',
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    placement: {
+      type: String,
+      default: 'top'
+    },
+    content: {
+      type: String,
+      default: ''
+    },
+    width: {
+      type: [Number, String],
+      default: ''
+    }
+  },
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+  <style lang="scss">
+  .tips-tooltip-main {
+    max-width: 60vw !important;
+  }
+  </style>
---
+++ src/views/work/todo/constants.ts
@@ -0,0 +1,6 @@
+export const STATUS_OPTIONS = [
+    { label: '未开始', value: 0 },
+    { label: '进行中', value: 1 },
+    { label: '已完成', value: 2 },
+    { label: '已取消', value: 3 }
+];
